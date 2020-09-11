8f136086ae858a7e74e0f7a8c71870efc300943b
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2020-09-11 15:25:22 +0200

    feat: add UI for dialogs

--- app/src/main/java/edu/mahmoudmabrok/gadsboard/ui/dialogs/Dialogs.kt
+++ app/src/main/java/edu/mahmoudmabrok/gadsboard/ui/dialogs/Dialogs.kt
@@ -1,11 +1,46 @@
 package edu.mahmoudmabrok.gadsboard.ui.dialogs
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.view.LayoutInflater
+import edu.mahmoudmabrok.gadsboard.R
+import kotlinx.android.synthetic.main.check_submit.view.*
 
 object Dialogs {
 
 
+    @SuppressLint("InflateParams")
     fun yesDialog(ctx: Context, yesAction: (Unit) -> Unit) {
+        val view = LayoutInflater.from(ctx).inflate(R.layout.check_submit, null)
 
+        val builder = AlertDialog.Builder(ctx)
+            .setView(view)
+            .setCancelable(false)
+            .create()
+
+        view.imClose?.setOnClickListener {
+            builder.dismiss()
+        }
+        view.btnYes?.setOnClickListener {
+            yesAction.invoke(Unit)
+            builder.dismiss()
+        }
+        builder.show()
+    }
+
+    @SuppressLint("InflateParams")
+    fun stateDialog(ctx: Context, isSuccess: Boolean = true) {
+        val resid = when (isSuccess) {
+            true -> R.layout.success_submit
+            else -> R.layout.fail_submit
+        }
+
+        val view = LayoutInflater.from(ctx).inflate(resid, null)
+
+        val builder = AlertDialog.Builder(ctx)
+            .setView(view)
+            .create()
+        builder.show()
     }
 }
--- app/src/main/java/edu/mahmoudmabrok/gadsboard/ui/submit/SubmitActivity.kt
+++ app/src/main/java/edu/mahmoudmabrok/gadsboard/ui/submit/SubmitActivity.kt
@@ -3,19 +3,17 @@ package edu.mahmoudmabrok.gadsboard.ui.submit
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.mahmoudmabrok.gadsboard.R
+import edu.mahmoudmabrok.gadsboard.ui.dialogs.Dialogs
 import kotlinx.android.synthetic.main.activity_sbmit.*
 
 
 class SubmitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_sbmit)
-        back?.setOnClickListener {
-            onBackPressed()
-        }
 
         // make it full screen
         window.decorView.systemUiVisibility = (
@@ -23,5 +21,16 @@ class SubmitActivity : AppCompatActivity() {
                         or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
         // make status ba transparent so it display  background
         window.statusBarColor = Color.TRANSPARENT
+
+
+        back?.setOnClickListener {
+            onBackPressed()
+        }
+        btnSubmit?.setOnClickListener {
+            Dialogs.yesDialog(this) {
+                Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
