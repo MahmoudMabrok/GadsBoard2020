42ddf361d7eca23df25876a177ab3138e2398774
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2020-09-11 14:42:56 +0200

    fix: issue with scrolling of tabs + viewpager add scroll flags to tab layout so when user scroll it be completely scroll

--- app/src/main/java/edu/mahmoudmabrok/gadsboard/ui/main/MainActivity.kt
+++ app/src/main/java/edu/mahmoudmabrok/gadsboard/ui/main/MainActivity.kt
@@ -3,7 +3,6 @@ package edu.mahmoudmabrok.gadsboard.ui.main
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import edu.mahmoudmabrok.gadsboard.R
 import edu.mahmoudmabrok.gadsboard.ui.submit.SubmitActivity
@@ -15,10 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
+        view_pager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        tabs.setupWithViewPager(view_pager)
 
         submit?.setOnClickListener {
             startActivity(Intent(this, SubmitActivity::class.java))
